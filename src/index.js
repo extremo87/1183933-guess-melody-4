563854982fe81d6a0d3c929445f3d49f6ae0d7bd
Sergey Ukhanov <ukhanov.sergey@gmail.com>
2020-06-09 15:46:41 +0600

    Components: welcome & app + webpack config fixes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/app';
+
+const config = {
+  errors: {
+    limit: 3
+  }
+};
+
+const init = () => {
+  ReactDOM.render(
+      <App errors={config.errors.limit} />,
+      document.querySelector(`#root`)
+  );
+};
+
+init();
 
