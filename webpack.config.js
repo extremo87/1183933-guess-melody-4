fa628ceddd0d2ec4e29073f0bd17af08264931e5
Sergey Ukhanov <ukhanov.sergey@gmail.com>
2020-06-09 11:28:24 +0600

    Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require(`path`);
+// eslint-disable-next-line no-undef
+const publicPath = path.join(__dirname, `public`);
+
+module.exports = {
+  entry: `./src/index.js`,
+  output: {
+    filename: `bundle.js`,
+    path: publicPath
+  },
+  devServer: {
+    contentBase: publicPath,
+    open: false,
+    port: 1337,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: `babel-loader`,
+        },
+      }
+    ],
+  },
+  devtool: `source-map`,
+};
